85d8daa6af0cd5d9d986036168b4269a73db63d2
caelansar <819711623@qq.com>
2024-02-06 20:27:01 +0800

    feat: handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
-use std::io::{BufRead, BufReader, Read};
-use std::{io::Write, net::TcpListener};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
+    while let Ok(stream) = listener.accept().await {
         match stream {
-            Ok(mut stream) => {
+            (mut stream, _) => {
                 println!(
                     "accepted new connection, addr {}",
                     stream.peer_addr().unwrap()
                 );
 
-                let mut buf = [0; 512];
-                while let Ok(_) = stream.read(&mut buf) {
-                    stream.write_all("+PONG\r\n".as_bytes()).unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
+                tokio::spawn(async move {
+                    let mut buf = [0; 512];
+                    while let Ok(_) = stream.read(&mut buf).await {
+                        stream.write_all("+PONG\r\n".as_bytes()).await.unwrap();
+                    }
+                });
             }
         }
     }
